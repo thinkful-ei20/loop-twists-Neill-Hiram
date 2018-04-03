da9530d8610b96ea5ab00ae8e2a8f14ddd21af88
Hiram Cruz <hiram0101@gmail.com>
2018-04-03 11:50:52 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function max(numbers) {
+  let maxNum = numbers[0];
+  let iter = 0;
+  while (iter < numbers.length) {
+    maxNum = numbers[iter] > maxNum ? numbers[iter] : maxNum;
+    iter++;
+  }
+  return maxNum;
+}
+
+function min(numbers) {
+  let minNum = numbers[0];
+  let iter = 0;
+  while (iter < numbers.length) {
+    minNum = numbers[iter] < minNum ? numbers[iter] : minNum;
+    iter++;
+  }
+  return minNum;
+}
